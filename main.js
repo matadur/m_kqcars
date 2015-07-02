3e69358a3c524b9c62c80a140428270780899c89
Nils Dannemann <ndannemann@gmail.com>
2015-07-02 16:00:40 +0200

    Collections, Grid, Products-List & Detail view, mixed changes

--- main.js
+++ main.js
@@ -1,5 +1,61 @@
 if (Meteor.isClient) {
 
+	// PRODUCTS 
+
+		// PRODUCTS - DISPLAY
+		Template.products.helpers({
+			products: function(){
+				return Products.find();
+			}
+		});
+
+		// PRODUCTS - ADD
+		Template.form_products_add.events({
+			'submit .form--products_add': function(event){
+				var model 		= event.target.model.value;
+				var kilometers 	= event.target.kilometers.value;
+				var built 		= event.target.built.value;
+				var wof 		= event.target.wof.value;
+				var reg 		= event.target.reg.value;
+				var price 		= event.target.price.value;
+				var description = event.target.description.value;
+				var email 		= event.target.email.value;
+				var number 		= event.target.number.value;
+
+				Products.insert({
+					model: model,
+					kilometers: kilometers,
+					built: built,
+					wof: wof,
+					reg: reg,
+					price: price,
+					description: description,
+					email: email,
+					number: number
+				});
+
+				event.preventDefault();
+
+				event.target.model.value = "";
+				event.target.kilometers.value = "";
+				event.target.built.value = "";
+				event.target.wof.value = "";
+				event.target.reg.value = "";
+				event.target.price.value = "";
+				event.target.description.value = "";
+				event.target.email.value = "";
+				event.target.number.value = "";
+
+				Router.go('/');
+			}
+		});
+
+		// PRODUCTS - REMOVE
+		Template.product_list.events({
+			'click .product__button--remove': function() {
+				Products.remove(this._id);
+			}
+		});
 }
 
 if (Meteor.isServer) {
