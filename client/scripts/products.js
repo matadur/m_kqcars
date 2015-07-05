f8dc6539c8726881cfa76adc82af1602139e3e38
Nils Dannemann <ndannemann@gmail.com>
2015-07-05 17:16:29 +0200

    Begun Solution for Image-Product-Relationship
    Images & Products are tied to a user via userId.
    Images & Products are tied to another via a shared product number.
    The product number is generated on route to add-form.

--- client/scripts/products.js
+++ client/scripts/products.js
@@ -1,3 +1,46 @@
+// IMAGE-PRODUCT-RELATION-HOOK
+// Count user products when add-form-route gets called
+// Add this number to images and the product to connect them
+function productNumber() {
+	var currentUser = Meteor.userId();
+	var productCount = Products.find({owner: currentUser}).count();
+	productCount++;
+
+	Session.set('productNumber', productCount);
+	this.next();
+}
+Router.onRun(productNumber, {only: ['my_products_add']});
+
+
+
+
+// IMAGES - UPLOAD
+Template.form_products_add.events({
+	'change .fileinput': function(event, template) {
+		var productNumber = Session.get('productNumber');
+
+		FS.Utility.eachFile(event, function(file) {
+			var tmpdoc = new FS.File(file);
+			tmpdoc.productNumber = productNumber;
+			tmpdoc.imageType = "sideimage";
+
+			Images.insert(tmpdoc, function (err) {
+				
+			});
+		});
+	}
+});
+
+// IMAGES - DISPLAY - SIDE
+Template.productfield_image_side.helpers({
+	sideimages: function () {
+		return Images.find({imageType: "sideimage"});
+	}
+});
+
+
+
+
 // PRODUCTS - DISPLAY
 Template.products.helpers({
 	products: function(){
@@ -8,18 +51,21 @@ Template.products.helpers({
 // PRODUCTS - ADD
 Template.form_products_add.events({
 	'submit .form--products_add': function(event){
-		var model 		= event.target.model.value;
-		var kilometers 	= event.target.kilometers.value;
-		var built 		= event.target.built.value;
-		var wof 		= event.target.wof.value;
-		var reg 		= event.target.reg.value;
-		var price 		= event.target.price.value;
-		var description = event.target.description.value;
-		var email 		= event.target.email.value;
-		var number 		= event.target.number.value;
-		var owner		= Meteor.userId();
+		var productNumber 	= Session.get('productNumber');
+		var owner		 	= Meteor.userId();
+		var model 			= event.target.model.value;
+		var kilometers 		= event.target.kilometers.value;
+		var built 			= event.target.built.value;
+		var wof 			= event.target.wof.value;
+		var reg 			= event.target.reg.value;
+		var price 			= event.target.price.value;
+		var description 	= event.target.description.value;
+		var email 			= event.target.email.value;
+		var number 			= event.target.number.value;
 
 		Products.insert({
+			productNumber: productNumber,
+			owner: owner,
 			model: model,
 			kilometers: kilometers,
 			built: built,
@@ -29,7 +75,6 @@ Template.form_products_add.events({
 			description: description,
 			email: email,
 			number: number,
-			owner: owner,
 			createdAt: new Date()
 		});
 
--- main.js
+++ main.js
@@ -4,29 +4,7 @@ if (Meteor.isClient) {
 	Meteor.subscribe('products');
 	Meteor.subscribe('images');
 
-	// IMAGES - UPLOAD
-	Template.form_products_add.events({
-		'change .fileinput': function(event, template) {
-			// Grab Project ID here with variables
-			FS.Utility.eachFile(event, function(file) {
-				// Construct FS.File document
-				var tmpdoc = new FS.File(file);
-				tmpdoc.productId = "1234";
-				tmpdoc.otherInfo = "sideview";
-				// Insert constructed document
-				Images.insert(tmpdoc, function (err) {
-					
-				});
-			});
-		}
-	});
-
-	// IMAGES - DISPLAY
-	Template.productfield_image_side.helpers({
-		images: function () {
-			return Images.find({otherInfo: "sideview"});
-		}
-	});
+	
 
 }
 
