11bd8dd3b7df6800aedb35ae90a02509b691cc28
Nils Dannemann <ndannemann@gmail.com>
2015-07-30 12:31:50 +0200

    [add] started pdf-download

--- client/components/product/product_controls.js
+++ client/components/product/product_controls.js
@@ -36,7 +36,7 @@
 			}
 		});
 
-	// PPRODUCT CONTROLS - DROPDOWN----------------------------------------------------
+	// PPRODUCT CONTROLS - DROPDOWN---------------------------------------------------
 		Template.product_controls.helpers({
 			toggleClass: function() {
 				var dropdownCurrentProduct = this._id;
@@ -58,5 +58,75 @@
 				} else{
 					Session.set('dropdownCurrentProductSession', null);
 				};
+			}
+		});
+
+	// PPRODUCT CONTROLS - PDF--------------------------------------------------------
+		Template.product_controls.events({
+			'click .button--pdf': function() {
+				var model 		= this.model;
+				var cartype 	= this.cartype;
+				var geartype 	= this.geartype;
+				var fueltype 	= this.fueltype;
+				var built 		= this.built;
+				var kilometers 	= this.kilometers;
+				var wof 		= this.wof;
+				var reg 		= this.reg;
+				var price 		= this.price;
+				var description = this.description;
+				var email 		= this.email;
+				var number 		= this.number;
+
+				// Define the pdf-document
+				var docDefinition = { 
+					pageSize: 'A4',
+					pageMargins: [ 35, 40, 35, 40 ],
+					
+					content: [
+						{ text: model, style: 'headline' },
+						{
+							columns: [
+								// Cartype
+								{ width: '15%', text: 'Cartype:', style: ['listItem', 'listLabel'] },
+								{ width: '35%', text: cartype, style: ['listItem', 'listText'] },
+								// Date built
+								{ width: '15%', text: 'Date built:', style: ['listItem', 'listLabel'] },
+								{ width: '35%', text: built, style: ['listItem', 'listText'] },
+							]
+						},
+						{
+							columns: [
+								// Geartype
+								{ width: '15%', text: 'Geartype:', style: ['listItem', 'listLabel'] },
+								{ width: '35%', text: geartype, style: ['listItem', 'listText'] },
+								// Kilometers
+								{ width: '15%', text: 'Kilometers:', style: ['listItem', 'listLabel'] },
+								{ width: '35%', text: kilometers, style: ['listItem', 'listText'] },
+							]
+						},
+						{
+							columns: [
+								// Fueltype
+								{ width: '15%', text: 'Fueltype:', style: ['listItem', 'listLabel'] },
+								{ width: '35%', text: fueltype, style: ['listItem', 'listText'] },
+								// Askingprice
+								{ width: '15%', text: 'Askingprice:', style: ['listItem', 'listLabel'] },
+								{ width: '35%', text: price, style: ['listItem', 'listText'] }
+							]
+						},
+						{ text: description, style: 'mainText' }
+					],
+
+					styles: {
+						headline: 	{ fontSize: 25, bold: true, margin: [0, 0, 0, 25] },
+						listItem:	{ fontSize: 12, margin: [0, 0, 0, 10] },
+						listLabel: 	{ bold: true },
+						listText: 	{ italic: true },
+						mainText: 	{ fontSize: 12, margin: [0, 15, 0, 15] }
+					}
+				};
+
+				// Start the pdf-generation process
+				pdfMake.createPdf(docDefinition).open();
 			}
 		});
