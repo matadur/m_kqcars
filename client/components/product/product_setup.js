4e6dd0c8c16c892bc4dfa75af9f3fdbf7e988260
Nils Dannemann <ndannemann@gmail.com>
2015-08-10 17:21:38 +0200

    [update] var functions

--- client/components/product/product_setup.js
+++ client/components/product/product_setup.js
@@ -3,7 +3,7 @@
 //------------------------------------------------------------------------------------
 
 	// SETUP - GENERATE A SHARED IMAGE-PRODUCT-ID-------------------------------------
-		function productCountId() {
+		var productCountId = function() {
 			var currentUser 	= Meteor.userId();
 			var productCount 	= Products.find({owner: currentUser}).count() + 1;
 			var productId 		= Math.floor(Math.random() * 123456789) + 1;
@@ -11,11 +11,11 @@
 
 			Session.set('productCountId', productCountId);
 			this.next(); // For Iron:Router
-		}
-		function removeSessionAfter() {
+		};
+		var removeSessionAfter = function() {
 			Session.set('productCountId', null);
-		}
-	
+		};
+
 	// SETUP - EXECUTE ON ROUTE-------------------------------------------------------
 		Router.onRun(productCountId, {only: ['products_add']});
 		Router.onStop(removeSessionAfter, {only: ['products_add']});
