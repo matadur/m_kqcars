0a8e74923f46231255c533fe8a3590ff052e347e
Nils Dannemann <ndannemann@gmail.com>
2015-08-14 13:05:34 +0200

    [update] completed product favorites

--- client/components/product/product_favorites.js
+++ client/components/product/product_favorites.js
@@ -5,12 +5,37 @@
 	// PRODUCT FAVORITES - DISPLAY COUNT----------------------------------------------
 		Template.sidebar_products.helpers({
 			product_favorite_count: function() {
-				var currentUser 	= Meteor.user();
-				var favorites 		= Meteor.user().profile.favorites;
-				var favoritesCount 	= favorites.length;
+				var currentUser 			= Meteor.user();
+				var currentFavorites 		= currentUser.profile.favorites;
+				var currentFavoritesCount 	= currentFavorites.length;
 
-				return favoritesCount;
+				return currentFavoritesCount;
 			} 
+		});
+
+	// PRODUCT FAVORITES - HEART ICON-------------------------------------------------
+		Template.product_favorite.helpers({
+			is_favorite: function(){
+				var currentUser 		= Meteor.user();
+				var currentFavorites 	= currentUser.profile.favorites;
+				var currentProduct 		= this._id;
+
+				if (currentFavorites.indexOf(currentProduct) != -1) {
+					return true;
+				} else {
+					return false;
+				};
+			}
+		});
+
+	// PRODUCT FAVORITES - DISPLAY PRODUCTS-------------------------------------------
+		Template.favorite_products.helpers({
+			favorite_products: function(){
+				var currentUser 		= Meteor.user();
+				var currentFavorites 	= currentUser.profile.favorites;
+
+				return Products.find({_id: {$in: currentFavorites}}, {sort: {createdAt: -1}});
+			}
 		});
 	
 	// PRODUCT FAVORITES - ADD TO USER------------------------------------------------
