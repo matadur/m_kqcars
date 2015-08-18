936440a9df739e331377870575f20b997df0d11b
Nils Dannemann <ndannemann@gmail.com>
2015-08-18 14:46:08 +0200

    [update] implemented "clear" filter

--- client/components/header/header_filter.js
+++ client/components/header/header_filter.js
@@ -2,8 +2,13 @@
 // HEADER - FILTER
 //------------------------------------------------------------------------------------
 	
-	// FILTER - PRICE-----------------------------------------------------------------
+	// FILTER ------------------------------------------------------------------------
 		Template.header_filter.events({
+			// Everything
+			'click .filter--none': function(event) {
+				Session.set('filter', null);
+			},
+			// Price
 			'click .filter--price': function(event) {
 				var filter = event.target.getAttribute('data-filter');
 				var filterNum = parseInt(filter);
