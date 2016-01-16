0fde23c1d0d5b3dc4620e010032a10db4dc93f44
Nils Dannemann <ndannemann@gmail.com>
2016-01-16 18:31:37 +0100

    [add] Form tooltips

--- client/lib/semantic_ui/semantic_ui.js
+++ client/lib/semantic_ui/semantic_ui.js
@@ -5,3 +5,15 @@
 	Template.product_controls.onRendered(function () {
 		$('.ui.dropdown').dropdown();
 	});
+
+	Template.product_formfield_wof.onRendered(function () {
+		$('.has-tooltip').popup({
+			hoverable: true,
+			position : 'bottom left',
+			delay: {
+				show: 190,
+				hide: 0
+			}
+		});
+	});
+	
