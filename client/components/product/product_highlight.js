72345a819bf4e539abd7defd1bb8b802ca35ea78
Nils Dannemann <ndannemann@gmail.com>
2016-01-24 11:27:41 +0100

    [add] first role based payment functionality -> for "Highlight Product"

--- client/components/product/product_highlight.js
+++ client/components/product/product_highlight.js
@@ -1,9 +1,60 @@
 //------------------------------------------------------------------------------------
 // PRODUCT HIGHLIGHT
 //------------------------------------------------------------------------------------
+	
+	// PAYMENT FORM - CONNECT TO BRAINTREE METHOD-------------------------------------
+		Template.products_highlight_instructions.onRendered(function() {
+			Meteor.call('getClientToken', function(error, clientToken) {
+				if (error) {
+					console.log(error);
+				} else {
+					braintree.setup(clientToken, "dropin", {
+						container: "payment-form--highlight", // Injecting into <div id="payment-form"></div>
+						onPaymentMethodReceived: function (response) {
+							// When we submit the payment form,
+							// it'll create new customer first...
+							var nonce = response.nonce;
 
-	Template.products_highlight_instructions.events({
-		'click .button--highlight': function() {
-			console.log("In progress");
-		}
-	});
+							Meteor.call('btCreateCustomer', function(error, success) {
+								if (error) {
+									throw new Meteor.Error('customer-creation-failed');
+								} else {
+									// ... and when the customer is successfuly created,
+									// call method for creating a transaction (finally!)
+									Meteor.call('createTransaction', nonce, function(error, success) {
+										if (error) {
+											throw new Meteor.Error('transaction-creation-failed');
+										} else {
+											alert('Thank you for your payment! Reload page to access our premium items!');
+										}
+									});
+								}
+							});
+						}
+					});
+				}
+			});
+		});
+
+	// PAYMENT FORM HELPER------------------------------------------------------------
+		Template.products_highlight_instructions.helpers({
+			paidHighlight: function() {
+				var userId = Meteor.userId();
+				var paidHighlight = Roles.userIsInRole(userId, 'paidHighlight');
+
+				if (paidHighlight == true) {
+					return true
+				} else {
+					return false
+				};
+			}
+		});
+
+	// HIGHLIGHT BUTTON--------------------------------------------------------------- (Needed?)
+		Template.products_highlight_instructions.events({
+			'click .button--highlight': function() {
+				console.log("In progress");
+			}
+		});
+
+	
--- server/methods.js
+++ server/methods.js
@@ -25,3 +25,92 @@
 				Images.remove({productCountId: productCountId});
 			}
 		});
+
+
+//------------------------------------------------------------------------------------
+// PAYMENTS
+//------------------------------------------------------------------------------------
+	
+	// SETUP ENVIRONMENT--------------------------------------------------------------
+		var gateway;
+
+		Meteor.startup(function () {
+			var env;
+			
+			// Pick Braintree environment based on environment defined in Meteor settings.
+			if (Meteor.settings.public.env === 'Production') {
+				env = Braintree.Environment.Production;
+			} else {
+				env = Braintree.Environment.Sandbox;
+			}
+			// Initialize Braintree connection:
+			gateway = BrainTreeConnect({
+				environment: env,
+				publicKey: Meteor.settings.public.BT_PUBLIC_KEY,
+				privateKey: Meteor.settings.private.BT_PRIVATE_KEY,
+				merchantId: Meteor.settings.public.BT_MERCHANT_ID
+			});
+		});
+
+	// DEFINE PAYMENT METHODS--------------------------------------------------------
+	Meteor.methods({
+		// CLIENT TOKEN
+		getClientToken: function (clientId) {
+			var generateToken = Meteor.wrapAsync(gateway.clientToken.generate, gateway.clientToken);
+			var options = {};
+
+			if (clientId) {
+				options.clientId = clientId;
+			}
+
+			var response = generateToken(options);
+			return response.clientToken;
+		},
+		// CREATE CUSTOMER ON BRAINTREE
+		btCreateCustomer: function(){
+			var user = Meteor.user();
+
+			var customerData = {
+				email: user.emails[0].address
+			};
+
+			// Calling the Braintree API to create our customer!
+			gateway.customer.create(customerData, function(error, response){
+				if (error){
+					console.log(error);
+				} else {
+					// If customer is successfuly created on Braintree servers,
+					// we will now add customer ID to our User
+					Meteor.users.update(user._id, {
+						$set: {
+							customerId: response.customer.id
+						}
+					});
+				}
+			});
+		},
+		// CREATE TRANSACTION
+		createTransaction: function(nonceFromTheClient) {
+			var user = Meteor.user();
+
+			// Let's create transaction.
+			gateway.transaction.sale({
+				amount: '10.00',
+				paymentMethodNonce: nonceFromTheClient, // Generated nonce passed from client
+				customer: {
+					id: user.customerId
+				},
+				options: {
+					submitForSettlement: true, // Payment is submitted for settlement immediatelly
+					storeInVaultOnSuccess: true // Store customer in Braintree's Vault
+				}
+			}, function (err, success) {
+				if (err) { 
+					console.log(err);
+				} else {
+					// When payment's successful, add "paid" role to current user.
+					Roles.addUsersToRoles(user._id, 'paidHighlight', Roles.GLOBAL_GROUP)
+				}
+			});
+		}
+	});
