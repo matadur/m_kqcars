dd3e317893b7a2efdd3f20f32bd1ff31c3558978
Nils Dannemann <ndannemann@gmail.com>
2016-01-31 07:29:21 +0100

    [update] notifications login with name

--- client/components/alerts/alerts.js
+++ client/components/alerts/alerts.js
@@ -16,3 +16,11 @@
 				onClose: _.noop
 			});
 		});
+
+		// Alert on Login
+		Accounts.onLogin(function() {
+			var currentUser = Meteor.user();
+			var currentUserName = currentUser.profile.name;
+
+			sAlert.success('Hi ' + currentUserName + '! Nice to see you!');
+		});
