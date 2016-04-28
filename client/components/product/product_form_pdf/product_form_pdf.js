b7b0b9990885f4e69208337a062ca0318738ad6d
Nils Dannemann <ndannemann@gmail.com>
2016-04-28 18:24:48 +0200

    [add] SSR pdf generation [refactor] meteor packages

--- client/components/product/product_form_pdf/product_form_pdf.js
+++ client/components/product/product_form_pdf/product_form_pdf.js
@@ -3,158 +3,16 @@
 //------------------------------------------------------------------------------------
 
 	Template.product_form_pdf.events({
-		'click .button--pdf': function() {
-			var clickedProductId = Session.get('clickedProductId');
-			var clickedProductObject = Products.findOne(clickedProductId);
-			var clickedProductCountId = Products.findOne(clickedProductId).productCountId;
-
-			var model 		= clickedProductObject.model.toString();
-			var cartype 	= clickedProductObject.cartype.toString();
-			var geartype 	= clickedProductObject.geartype.toString();
-			var fueltype 	= clickedProductObject.fueltype.toString();
-			var built 		= clickedProductObject.built.toString();
-			var kilometers 	= clickedProductObject.kilometers.toString();
-			var wof 		= clickedProductObject.wof.toString();
-			var reg 		= clickedProductObject.reg.toString();
-			var price 		= clickedProductObject.price.toString();
-			var description = clickedProductObject.description.toString();
-			var email 		= clickedProductObject.email.toString();
-			var number 		= clickedProductObject.number.toString();
-			
-			// IMAGES - Get Product Image Urls
-			var logoimage		= '/logo.png';
-			var backgroundimage	= '/pdf-bg.png';
-			var sideimage 		= Images.findOne({$and: [{productCountId: clickedProductCountId}, {imageType: "sideimage"}]}).url();
-			var frontimage 		= Images.findOne({$and: [{productCountId: clickedProductCountId}, {imageType: "frontimage"}]}).url();
-			var infrontimage	= Images.findOne({$and: [{productCountId: clickedProductCountId}, {imageType: "infrontimage"}]}).url();
-			var inbackimage 	= Images.findOne({$and: [{productCountId: clickedProductCountId}, {imageType: "inbackimage"}]}).url();
-
-			// IMAGES - Convert Image url to Data Uri
-			getDataUri = function (url, callback) {
-				var image = new Image();
-				image.onload = function () {
-					var canvas = document.createElement('canvas');
-					canvas.width = this.naturalWidth;
-					canvas.height = this.naturalHeight;
-					canvas.getContext('2d').drawImage(this, 0, 0);
-					callback(canvas.toDataURL());
-				};
-				image.src = url;
-			};
-
-			// DATA URI WRAPPER (doc definition has to be in the callback so that image is loaded completely $ converted to datauri)
-			getDataUri(logoimage, function(logoimageDataUri) {
-			getDataUri(backgroundimage, function(backgroundimageDataUri) {
-			getDataUri(sideimage, function(sideimageDataUri) {
-			getDataUri(frontimage, function(frontimageDataUri) {
-			getDataUri(infrontimage, function(infrontimageDataUri) {
-			getDataUri(inbackimage, function(inbackimageDataUri) {
-				// PDF DOCUMENT DEFINITION
-				var docDefinition = { 
-					pageSize: 'A4',
-					pageMargins: [ 35, 125, 35, 290 ],
-					background: [{image: backgroundimageDataUri, width: 595}],
-					
-					// header: [
-					// 	{image: logoimageDataUri, fit: [125,125], margin: [35, 0, 35, 0]}
-					// ],
-					
-					content: [
-						// MODEL
-						{ text: model, style: 'headline' },
-						// MODEL - CARTYPE & DATE BUILT
-						{
-							columns: [
-								// Cartype
-								{ width: '15%', text: 'Cartype:', style: ['listItem', 'listLabel'] },
-								{ width: '35%', text: cartype, style: ['listItem', 'listText'] },
-								// Date built
-								{ width: '15%', text: 'Date built:', style: ['listItem', 'listLabel'] },
-								{ width: '35%', text: built, style: ['listItem', 'listText'] },
-							]
-						},
-						// MODEL - GEARTYPE & KILOMETERS
-						{
-							columns: [
-								// Geartype
-								{ width: '15%', text: 'Geartype:', style: ['listItem', 'listLabel'] },
-								{ width: '35%', text: geartype, style: ['listItem', 'listText'] },
-								// Fueltype
-								{ width: '15%', text: 'Fueltype:', style: ['listItem', 'listLabel'] },
-								{ width: '35%', text: fueltype, style: ['listItem', 'listText'] }
-							]
-						},
-						// MODEL - KILOMETERS
-						{
-							columns: [
-								// Kilometers
-								{ width: '15%', text: 'Kilometers:', style: ['listItem', 'listLabel'] },
-								{ width: '35%', text: kilometers, style: ['listItem', 'listText'] },
-							]
-						},
-						// ABOUT
-						{ text: 'About the Car:', style: 'section' },
-						// ABOUT - DESCRIPTION
-						{ text: description, style: 'mainText' },
-					],
-
-					footer: [
-						// PRICE
-						{ text: 'Price:', style: 'section', margin: [35, 0, 35, 10] },
-						// PRICE - PRICE
-						{
-							columns: [
-								// Askingprice
-								{ width: '15%', text: 'Askingprice:', style: ['listItem', 'listLabel'] },
-								{ width: '35%', text: '$' + price + ' NZD (or near offer)', style: ['listItem', 'listText'] }
-							],
-							margin: [35, 0, 35, 25]
-						},
-						// CONTACT
-						{ text: 'Contact:', style: 'section', margin: [35, 0, 35, 10] },
-						// CONTACT - EMAIL & NUMBER
-						{
-							columns: [
-								// Email
-								{ width: '15%', text: 'Emailadress:', style: ['listItem', 'listLabel'] },
-								{ width: '35%', text: email, style: ['listItem', 'listText'] },
-								// Number
-								{ width: '15%', text: 'Number:', style: ['listItem', 'listLabel'] },
-								{ width: '35%', text: number, style: ['listItem', 'listText'] }
-							],
-							margin: [35, 0, 35, 25]
-						},
-						// IMAGES
-						{ text: 'Images:', style: 'section', margin: [35, 0, 35, 10] },
-						// IMAGES - IMAGES
-						{
-							columns: [
-								{ width: '25%', image: sideimageDataUri, fit: [125,125] },
-								{ width: '25%', image: frontimageDataUri, fit: [125,125] },
-								{ width: '25%', image: infrontimageDataUri, fit: [125,125] },
-								{ width: '25%', image: inbackimageDataUri, fit: [125,125] }
-							],
-							margin: [35, 0, 35, 0]
-						}
-					],
-
-					styles: {
-						headline: 	{ fontSize: 25, bold: true, margin: [0, 0, 0, 25] },
-						section: 	{ fontSize: 16, bold: true, margin: [0, 25, 0, 10] },
-						listItem:	{ fontSize: 10, margin: [0, 0, 0, 10] },
-						listLabel: 	{ bold: true },
-						listText: 	{ italic: true },
-						mainText: 	{ fontSize: 10, lineHeight: 1.5, margin: [0, 0, 0, 10] },
-						footerText:	{ fontSize: 10, lineHeight: 1.5, margin: [35, 0, 35, 0] }
-					}
-				};
-				// PDF GENERATION PROCESS
-				pdfMake.createPdf(docDefinition).open();
-			});
-			});
-			});
-			});
-			});
-			});
+		'click .button--pdf': function(e, tmpl) {
+			e.preventDefault();
+			Meteor.call('method_generate_pdf_basic', function(err, res) {
+				if (err) {
+					console.error(err);
+				} else if (res) {
+					window.open("data:application/pdf;base64, " + res);
+					// OPTIONAL: Open in same window
+					// window.location.assign("data:application/pdf;base64, " + res);
+				}
+			})
 		}
 	});
---
+++ server/method_generate_pdf_basic.js
@@ -0,0 +1,60 @@
+import webshot from 'webshot';
+import fs from 'fs';
+import Future from 'fibers/future';
+
+Meteor.methods({
+	'method_generate_pdf_basic': function() {
+		var fut = new Future();
+		var fileName = "pdf_basic.pdf";
+
+		// Generate HTML String
+		var css = Assets.getText('pdf_basic.css');
+
+		SSR.compileTemplate('layout_pdf_basic', Assets.getText('layout_pdf_basic.html'));
+
+		Template.layout_pdf_basic.helpers({
+			getDocType: function() {
+				return "<!DOCTYPE html>";
+			}
+		});
+		
+		SSR.compileTemplate('page_pdf_basic', Assets.getText('page_pdf_basic.html'));
+
+		// Get Data
+		var products = Products.find({});
+		var data = {
+			products: products
+		}
+
+		var html_string = SSR.render('layout_pdf_basic', {
+			css: css,
+			template: "page_pdf_basic",
+			data: data
+		});
+
+		// Setup Webshot Options
+		var options = {
+			"paperSize": {
+				"format": "Letter",
+				"orientation": "portrait",
+				"margin": "0cm"
+			},
+			siteType: 'html'
+		};
+
+		// Commence Webshot
+		console.log("Commencing webshot...");
+		webshot(html_string, fileName, options, function(err) {
+			fs.readFile(fileName, function (err, data) {
+				if (err) {return console.log(err);}
+				fs.unlinkSync(fileName);
+				fut.return(data);
+			});
+		});
+		
+		// Return PDF String
+		let pdfData = fut.wait();
+		let base64String = new Buffer(pdfData).toString('base64');
+		return base64String;
+	}
+});
