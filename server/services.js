9c7cc3dfefeb7d37c5da868b14cb8497decdbb52
Nils Dannemann <ndannemann@gmail.com>
2016-05-07 17:15:47 +0200

    [add] user avatar + [update] small changes

--- server/services.js
+++ server/services.js
@@ -1,4 +1,4 @@
-// Facebook
+// Service Config - Facebook
 ServiceConfiguration.configurations.remove({
 	service: 'facebook'
 });
@@ -9,7 +9,7 @@ ServiceConfiguration.configurations.insert({
 	secret: '8bb3a31063f67f36145e162dc5107eb2'
 });
 
-// Google
+// Service Config - Google
 ServiceConfiguration.configurations.remove({
 	service: 'google'
 });
@@ -20,21 +20,23 @@ ServiceConfiguration.configurations.insert({
 	secret: 'EQq4EvYf6uoqEfUb20JCaKXZ'
 });
 
-
+// Attach Data (email + image) to User
 Accounts.onCreateUser(function(options,user) {
-  const facebook = user.services.facebook ? user.services.facebook : false;
-  const google = user.services.google ? user.services.google : false;
+	const facebook = user.services.facebook ? user.services.facebook : false;
+	const google = user.services.google ? user.services.google : false;
 
-  if (options.profile) {
-    if (facebook) {
-       options.profile.email = facebook.email
-       options.profile.picture = "http://graph.facebook.com/" + facebook.id + "/picture/?type=large";
-    } else if (google) {
-      options.profile.email = google.email
-      options.profile.picture = google.picture
-    }
-  }
-  
-  user.profile = options.profile;
-  return user;
-})
+	if (options.profile) {
+	    //Facebook
+		if (facebook) {
+			options.profile.email = facebook.email
+			options.profile.picture = "http://graph.facebook.com/" + facebook.id + "/picture/?type=large";
+		//Google
+		} else if (google) {
+			options.profile.email = google.email
+			options.profile.picture = google.picture
+		}
+	}
+	
+	user.profile = options.profile;
+	return user;
+});
